af7852514688e75d1e53d2915443a7342cf9f3c3
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2026-06-25 09:55:54 -0400

    (chirper): include auth and policies

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -43,10 +43,7 @@ class ChirpController extends Controller
         ]);
 
         // Store Request
-        Chirp::create([
-            'message' => $validated['message'],
-            'user_id' => null,
-        ]);
+        auth()->user()->chirps()->create($validated);
 
         // Redirect to home with Success
         return redirect('/')->with('success', 'Your chirp has been posted!');
@@ -65,6 +62,7 @@ class ChirpController extends Controller
      */
     public function edit(Chirp $chirp)
     {
+        $this->authorize('update', $chirp);
         return view('chirps.edit', compact('chirp'));
     }
 
@@ -73,6 +71,8 @@ class ChirpController extends Controller
      */
     public function update(Request $request, Chirp $chirp)
     {
+       $this->authorize('update', $chirp);
+
        // Validate
        $validated = $request->validate([
            'message' => 'required|string|max:255',
@@ -89,6 +89,7 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
+       $this->authorize('delete', $chirp);
        $chirp->delete();
        return redirect('/')->with('success', 'Chirp deleted!');
     }
